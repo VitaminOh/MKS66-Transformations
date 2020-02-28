0766010255ff33e581fc796e1268953f31c824c5
VitaminOh <dlin01@stuy.edu>
2020-02-28 13:48:51 -0500

    fixed rotations and translation

--- matrix.py
+++ matrix.py
@@ -10,22 +10,22 @@ z0  z1  ... zn
 import math
 
 def make_translate( x, y, z ):
-    return [[1, 0, 0, x], [0, 1, 0, y], [0, 0, 0, z], [0, 0, 0, 1]]
+    return [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [x, y, z, 1]]
 
 def make_scale( x, y, z ):
     return [[x, 0, 0, 0], [0, y, 0, 0], [0, 0, z, 0], [0, 0, 0, 1]]
 
 def make_rotX( theta ):
     radians = math.radians(theta)
-    return [[1, 0, 0, 0], [0, math.cos(radians), -1 * math.sin(radians), 0], [0, math.sin(radians), math.cos(radians), 0], [0, 0, 0, 1]]
+    return [[1, 0, 0, 0], [0, math.cos(radians), math.sin(radians), 0], [0, -1 * math.sin(radians), math.cos(radians), 0], [0, 0, 0, 1]]
 
 def make_rotY( theta ):
     radians = math.radians(theta)
-    return [[math.cos(radians), 0, math.sin(radians), 0], [0, 1, 0, 0], [-1 * math.sin(radians), 0, math.cos(radians), 0], [0, 0, 0, 1]]
+    return [[math.cos(radians), 0, -1 * math.sin(radians), 0], [0, 1, 0, 0], [math.sin(radians), 0, math.cos(radians), 0], [0, 0, 0, 1]]
 
 def make_rotZ( theta ):
     radians = math.radians(theta)
-    return [[math.cos(radians), -1 * math.sin(radians), 0, 0], [math.sin(radians), math.cos(radians), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+    return [[math.cos(radians), math.sin(radians), 0, 0], [-1 * math.sin(radians), math.cos(radians), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 
 #rounds the values of the matrix to the floor int
 def round_matrix(matrix):
